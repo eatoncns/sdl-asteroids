96768125439470c85f62548afddcd2a9f3f08ae0
Phil <philip.meier06@googlemail.com>
2015-08-30 22:19:20 +0100

    Retroactively add tests for Vector class (oops)

---
+++ asteroids/src/gameTest/cpp/VectorTest.cpp
@@ -0,0 +1,32 @@
+#include <gmock/gmock.h>
+#include <Vector.hpp>
+#include <sstream>
+
+using namespace pjm;
+
+TEST(VectorTest, InitialisesMembers)
+{
+    Vector v(5.0f, 7.0f);
+    EXPECT_EQ(5.0f, v.x);
+    EXPECT_EQ(7.0f, v.y);
+}
+
+TEST(VectorTest, ImplementsEquals)
+{
+    Vector v1(2.0f, 4.0f);
+    Vector v2(3.0f, 4.0f);
+    Vector v3(2.0f, 5.0f);
+    Vector v4(7.0f, 9.0f);
+    EXPECT_TRUE(v1 == v1);
+    EXPECT_FALSE(v1 == v2);
+    EXPECT_FALSE(v1 == v3);
+    EXPECT_FALSE(v1 == v4);
+}
+
+TEST(VectorTest, PrettyPrints)
+{
+    Vector v(5.2f, 7.9f);
+    std::stringstream ss;
+    ss << v;
+    EXPECT_EQ("(5.2,7.9)", ss.str());
+}
